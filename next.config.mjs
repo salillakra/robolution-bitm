f1d51f07e967a6037e99839a2bae5b0cb4c4a7e1
Salil Lakra <salillakra.dev@gmail.com>
2025-12-14 12:51:38 +0530

    feat: Implement core frontend pages and content management with new collections, components, and utilities.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
